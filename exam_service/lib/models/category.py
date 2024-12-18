04793345964ae54b8346fe9deeaedfdb04e3d66b
Arthur <baginian.artur@yandex.ru>
2024-12-18 14:30:16 +0300

    Some minor fixes

--- exam_service/lib/models/category.py
+++ exam_service/lib/models/category.py
@@ -1,21 +1,11 @@
-"""Problem model."""
-
-from typing import TYPE_CHECKING
-
-from sqlalchemy import ForeignKey, Integer
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Integer
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .abc import AbstractModel
 
 
-
-class ProblemModel(AbstractModel):
-    """The problems model."""
-
+class CategoryModel(AbstractModel):
     __tablename__ = "categories"
     id: Mapped[int] = mapped_column("id", Integer(), primary_key=True, autoincrement=True)
-    """ID of the problem."""
     name: Mapped[str]
-    """Text of the problem."""
-    category_id: Mapped[int] = mapped_column(ForeignKey("exams.id"))
-    """ID of the associated exam, FK."""
+
--- exam_service/lib/models/exam.py
+++ exam_service/lib/models/exam.py
@@ -1,20 +1,13 @@
-"""Exam model."""
-
-from typing import TYPE_CHECKING
-
-from sqlalchemy import Integer
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .abc import AbstractModel
 
 
 class ExamModel(AbstractModel):
-    """Exams model."""
-
     __tablename__ = "exams"
     id: Mapped[int] = mapped_column("id", Integer(), primary_key=True, autoincrement=True)
-    """ID of the exam."""
     name: Mapped[str]
-    """Name of the exam."""
-    category: Mapped[str]
-    """Category of exam"""
+    description: Mapped[str | None]
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"))
+
