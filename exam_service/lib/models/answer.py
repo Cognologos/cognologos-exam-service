6d7b12dd5346f2a2cc5c2a56e238487f8ea48587
Vlad-Note <gricenkovlad007@gmail.com>
2024-12-20 11:12:04 +0300

    add relationships between result and answer

--- exam_service/lib/models/answer.py
+++ exam_service/lib/models/answer.py
@@ -1,12 +1,17 @@
 from sqlalchemy import ForeignKey, Integer
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import TYPE_CHECKING
 
 from .abc import AbstractModel
+
+if TYPE_CHECKING:
+    from .result import ResultModel
 
 
 class AnswerModel(AbstractModel):
     __tablename__ = "answers"
     id: Mapped[int] = mapped_column("id", Integer(), primary_key=True, autoincrement=True)
     question_id: Mapped[int] = mapped_column(ForeignKey("questions.id"))
-    result_id: Mapped[int] = mapped_column(ForeignKey("result.id"))
+    result_id: Mapped[int] = mapped_column(ForeignKey("results.id"))
     answer: Mapped[str]
+    results: Mapped[list["ResultModel"]] = relationship("ResultModel", back_populates="results")
--- exam_service/lib/models/result.py
+++ exam_service/lib/models/result.py
@@ -1,11 +1,14 @@
 from sqlalchemy import ForeignKey, Integer
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import TYPE_CHECKING
 
 from .abc import AbstractModel
+if TYPE_CHECKING:
+    from .answer import AnswerModel
 
 
 class ResultModel(AbstractModel):
     __tablename__ = "results"
     id: Mapped[int] = mapped_column("id", Integer(), primary_key=True, autoincrement=True)
     exam_id: Mapped[int] = mapped_column(ForeignKey("exams.id"))
-    result: Mapped[str]
+    answers: Mapped[list["AnswerModel"]] = relationship("AnswerModel", back_populates="answers")
