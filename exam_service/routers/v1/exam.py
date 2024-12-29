3b34045b9e9f0059ee2189b12f79ddc0bb6ddd81
Vlad-Note <gricenkovlad007@gmail.com>
2024-12-30 00:14:16 +0300

    Add delete exam, crud complete (need testing)

--- exam_service/routers/v1/exam.py
+++ exam_service/routers/v1/exam.py
@@ -16,3 +16,8 @@ async def create_exam(db: DatabaseDependency, schema: ExamCreateSchema) -> ExamS
 @router.get("/get_exam", response_model=ExamSchema)
 async def get_user(db: DatabaseDependency, exam_id: int) -> ExamSchema:
     return await exam_db.get_exam(db, exam_id=exam_id)
+
+
+@router.delete("/get_exam", status_code=204)
+async def delete_exam(db: DatabaseDependency, exam_id: int) -> None:
+    return await exam_db.delete_exam(db, exam_id=exam_id)
